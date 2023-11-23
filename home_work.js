6de0a367a812dbec799788405e1d8d7382598982
Denys Dudnik <dudnikdenys777@gmail.com>
2023-11-23 23:23:39 +0200

    add barnch js_homework2

--- home_work.js
+++ home_work.js
@@ -5,3 +5,4 @@ var user_number = prompt('Enter your number: ')
 var result = isNaN(user_number) ? 'Type Error: Please, enter a number' : Number(user_number) % 2 === 0 ? 'even number' : 'odd number'
 
 console.log(`Number "${user_number}" - ${result}`)
+
