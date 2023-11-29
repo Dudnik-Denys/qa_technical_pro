8d9b995cf02f5c6e72889e484345009c4c717785
Denys Dudnik <dudnikdenys777@gmail.com>
2023-11-29 19:06:12 +0200

    fix hw

--- myIsNaN.js
+++ myIsNaN.js
@@ -1,8 +1,5 @@
 function myIsNaN(value) {
-    if (typeof(value) === Boolean || value === null) {
-        return false
-    }
-    if (value.valueOf() === Number(value)) {
+    if (typeof(value) === Boolean || value === null || value.valueOf() === Number(value)) {
         return false
     }
     return +value != value
