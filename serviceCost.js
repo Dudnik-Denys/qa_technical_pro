3dc9fd6741e47dabad010109d3b9fd1e72e4470a
Denys Dudnik <dudnikdenys777@gmail.com>
2023-12-04 16:51:40 +0200

    small fix

--- serviceCost.js
+++ serviceCost.js
@@ -25,7 +25,7 @@ var services = {
                 minimum = cost
             }
         }
-        return minimum
+        return this.prices.length > 0 ? minimum : 0
     },
     maxPrice: function () {
         maximum = 0
